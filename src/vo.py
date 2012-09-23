75881033f214e22d030989fefb41287d22267965
Dave Wilson <dave.wilson1@ntlworld.com>
2012-09-23 12:16:31 +0100

    modified to use observer.

--- src/vo.py
+++ src/vo.py
@@ -2,29 +2,58 @@
 PureMVC Python Demo - wxPython Employee Admin 
 By Toby de Havilland <toby.de.havilland@puremvc.org>
 Copyright(c) 2007-08 Toby de Havilland, Some rights reserved.
+
+Modified by Dave Wilson
 """
 
+from obs_class_attr import ObsClassAttr, obs_any, AUTO_TOPIC
 import enum
 
 class RoleVO(object):
+    
+    user_name = ObsClassAttr("user_name")
+    roles = ObsClassAttr("roles")
+    
     def __init__(self, user_name=None, roles=None):
         self.user_name = user_name
         self.roles = roles or []
 
 class UserVO(object):
-    def __init__(self, user_name=None, first_name=None, last_name=None,
-                 email=None, password=None, department=None):
+    
+    user_name = ObsClassAttr("user_name")
+    first_name = ObsClassAttr("first_name")
+    last_name = ObsClassAttr("last_name")
+    email = ObsClassAttr("email")
+    password = ObsClassAttr("password")
+    is_valid = ObsClassAttr("is_valid")
+    given_name = ObsClassAttr("given_name")
+
+    def __init__(self, user_name="", first_name="", last_name="",
+                 email="", password="", department=""):
         self.user_name = user_name
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.password = password
         self.department = department or enum.DEPT_NONE_SELECTED
-
-    def is_valid(self):
-        return all([self.user_name, self.password,
-                    self.department != enum.DEPT_NONE_SELECTED]
-                   )
+        self.is_valid = False
+        self.given_name = ""
+        obs_any(self, self.any_changed)
+        self.calc_given_name()
+        self.calc_is_valid()
+        
+    def any_changed(self, value, topic=AUTO_TOPIC):#IGNORE:W0613
+        topic = topic.getNameTuple()
+        if topic[1] in ("first_name", "last_name"):
+            self.calc_given_name()
+        if topic[1] not in ("is_valid", "given_name"):
+            self.calc_is_valid()
+        
+    def calc_is_valid(self):
+        self.is_valid =  all([self.user_name, self.password,
+                              self.department != enum.DEPT_NONE_SELECTED])
     
-    def given_name(self):
-        return self.last_name + ', ' + self.first_name
+    def calc_given_name(self):
+        self.given_name =  "%s, %s" % (self.last_name, self.first_name)
+        
+
