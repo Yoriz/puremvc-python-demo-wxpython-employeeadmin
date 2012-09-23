2ccd72bf7c6db82865495441b2f9d06507aa15a1
Dave Wilson <dave.wilson1@ntlworld.com>
2012-09-23 22:13:40 +0100

    Removed the ordinal.

--- src/employee_admin/model/enum/dept_enum.py
+++ src/employee_admin/model/enum/dept_enum.py
@@ -4,20 +4,12 @@ Created on 23 Sep 2012
 @author: Dave Wilson
 '''
 
-from collections import namedtuple
 
-DeptEnumItem = namedtuple("DeptEnumItem", "value, ordinal")
-  
-DEPT_NONE_SELECTED = DeptEnumItem("--None Selected--" , -1)
-DEPT_ACCT = DeptEnumItem("Accounting" , 0)
-DEPT_SALES = DeptEnumItem("Sales" , 1)
-DEPT_PLANT = DeptEnumItem("Plant" , 2)
+DEPT_NONE_SELECTED = "--None Selected--" 
+DEPT_ACCT = "Accounting"
+DEPT_SALES = "Sales"
+DEPT_PLANT = "Plant"
 
-dept_list = [DEPT_ACCT, DEPT_SALES, DEPT_PLANT]
-
-dept_combo_list = dept_list[:]
-dept_combo_list.insert(0, DEPT_NONE_SELECTED)
-
-
+dept_list = [DEPT_NONE_SELECTED, DEPT_ACCT, DEPT_SALES, DEPT_PLANT]
 
     
